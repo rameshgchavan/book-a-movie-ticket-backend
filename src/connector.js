fd75393b6efa8c223259ddd2e4a261aab74f3592
Ramesh_Chavan <ramesh7452@gmail.com>
2023-07-19 12:59:24 +0530

    model returned by mongoose connection

--- src/connector.js
+++ src/connector.js
@@ -5,12 +5,16 @@ const mongoose = require('mongoose');
 // Imported bookMovieSchema schema
 const { bookMovieSchema } = require('./schema');
 // To establish mongoose server connection
-await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => { console.log("connection established with mongodb server online"); })
+const collection_connection = mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("connection established with mongodb server online");
+        // Model created using book movie schema
+        return mongoose.model('bookmovietickets', bookMovieSchema);
+    })
     .catch(err => {
         console.log("error while connection", err)
     });
 // Model created using book movie schema
-const collection_connection = mongoose.model('bookmovietickets', bookMovieSchema);
+// const collection_connection = mongoose.model('bookmovietickets', bookMovieSchema);
 // Exported model 
 exports.connection = collection_connection;
