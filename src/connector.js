56b3499bf77e41f5efa7dac2186a49062540638c
Ramesh_Chavan <ramesh7452@gmail.com>
2023-07-19 15:54:24 +0530

    Commented extra code

--- src/connector.js
+++ src/connector.js
@@ -3,14 +3,14 @@ const mongoURI = "mongodb://0.0.0.0:27017/" + "bookMovie";
 // Imported mongoose
 const mongoose = require('mongoose');
 // Imported bookMovieSchema schema
-const { bookMovieSchema } = require('./schema');
+// const { bookMovieSchema } = require('./schema');
 // To establish mongoose server connection
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => { console.log("connection established with mongodb server online"); })
     .catch(err => {
         console.log("error while connection", err)
     });
-// Model created using book movie schema
-const collection_connection = mongoose.model('bookmovietickets', bookMovieSchema);
-// Exported model 
-exports.connection = collection_connection;
+// // Model created using book movie schema
+// const collection_connection = mongoose.model('bookmovietickets', bookMovieSchema);
+// // Exported model 
+// exports.connection = collection_connection;
