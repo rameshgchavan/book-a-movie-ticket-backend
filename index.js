3f431271a107af487e7172fd67ef7af2b67a68f6
Ramesh_Chavan <ramesh7452@gmail.com>
2023-07-19 17:51:00 +0530

    connector code added inside index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-require("./src/connector");
+// require("./src/connector");
 const mongoURI = "mongodb://127.0.0.1:27017/" + "bookMovie";
 // Imported mongoose
 const mongoose = require('mongoose');
-// Imported bookMovieSchema schema
-const { bookMovieSchema } = require('./schema');
 // To establish mongoose server connection
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => { console.log("connection established with mongodb server online"); })
