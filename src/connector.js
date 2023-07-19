bb4d9f4c7b5b35149f69efb1e88686da420d2071
Ramesh_Chavan <ramesh7452@gmail.com>
2023-07-19 12:48:08 +0530

    await added before mongoose.connect

--- src/connector.js
+++ src/connector.js
@@ -1,16 +1,16 @@
 // Local mongoDB URI with collection name
-const mongoURI = "mongodb://0.0.0.0:27017/" + "bookMovie"
+const mongoURI = "mongodb://0.0.0.0:27017/" + "bookMovie";
 // Imported mongoose
 const mongoose = require('mongoose');
 // Imported bookMovieSchema schema
-const { bookMovieSchema } = require('./schema')
+const { bookMovieSchema } = require('./schema');
 // To establish mongoose server connection
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => { console.log("connection established with mongodb server online"); })
     .catch(err => {
         console.log("error while connection", err)
     });
 // Model created using book movie schema
-const collection_connection = mongoose.model('bookmovietickets', bookMovieSchema)
+const collection_connection = mongoose.model('bookmovietickets', bookMovieSchema);
 // Exported model 
 exports.connection = collection_connection;
