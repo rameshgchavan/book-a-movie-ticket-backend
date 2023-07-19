b58ce3518957c5f023ed0b0148b7a294d80aca08
Ramesh_Chavan <ramesh7452@gmail.com>
2023-07-19 22:50:41 +0530

    extra code removed

--- src/connector.js
+++ src/connector.js
@@ -5,16 +5,14 @@ dotEnv.config();
 // const mongoURI = process.env.MONGODB_URL;
 const mongoURI = "mongodb://127.0.0.1:27017/" + "bookMovie";
 // Imported mongoose
-let mongoose = require('mongoose');
-// Imported bookMovieSchema schema
-const { bookMovieSchema } = require('./schema');
+const mongoose = require('mongoose');
+
 // To establish mongoose server connection
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+const mongoDB = mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => { console.log("connection established with mongodb server online"); })
     .catch(err => {
         console.log("error while connection", err)
     });
-// Model created using book movie schema
-let collection_connection = mongoose.model('bookmovietickets', bookMovieSchema);
+
 // Exported model 
-exports.connection = collection_connection;
+exports.connection = mongoDB;
