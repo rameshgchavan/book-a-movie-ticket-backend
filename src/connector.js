67ba6c41b0d97990fa733dcf12b88faf32848b5f
Ramesh_Chavan <ramesh7452@gmail.com>
2023-07-20 00:31:21 +0530

    simplyfied code

--- src/connector.js
+++ src/connector.js
@@ -1,18 +1,14 @@
-require('mongodb');
-// Local mongoDB URI with collection name
+// setting environment to get MongoDB_URL
 const dotEnv = require("dotenv");
 dotEnv.config();
 const mongoURI = process.env.MONGODB_URL;
-// const mongoURI = "mongodb://localhost:27017/" + "bookMovie";
+
 // Imported mongoose
 const mongoose = require('mongoose');
 
-// To establish mongoose server connection
-const mongoDB = mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+// Establish mongoose server connection and exported
+module.exports = mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => { console.log("connection established with mongodb server online"); })
     .catch(err => {
         console.log("error while connection", err)
     });
-
-// Exported model 
-exports.connection = mongoDB;
