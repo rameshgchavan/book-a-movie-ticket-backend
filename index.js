e52bdcd1b92dfd21691ddea9e55dbb6061fa5b2d
Ramesh_Chavan <ramesh7452@gmail.com>
2023-07-19 18:05:52 +0530

    garbage code removed

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
-// require("./src/connector");
-const mongoURI = "mongodb://127.0.0.1:27017/" + "bookMovie";
-// Imported mongoose
-const mongoose = require('mongoose');
-// To establish mongoose server connection
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => { console.log("connection established with mongodb server online"); })
-    .catch(err => {
-        console.log("error while connection", err)
-    });
-// Model created using book movie schema
-
-mongoose.connection.on('connected', () => {
-    console.log('Mongoose is connected.')
-});
-
-mongoose.connection.on('error', (err) => {
-    console.log(`Mongoose not connected: ${err}`)
-});
+require("./src/connector");
 // Imported express
 const express = require("express");
 // Created express object
