7a894761ec7fc0e5c1bef5cadce60bc1443ed540
Ramesh_Chavan <ramesh7452@gmail.com>
2023-07-20 00:30:40 +0530

    re-arranged code

--- src/api.js
+++ src/api.js
@@ -1,14 +1,13 @@
-const { connection } = require("./connector");
-
 // Imported express
 const express = require("express");
-// Imported connection model
-const { bookMovieModel } = require("./model");
 // Created express router
 const movieRouter = express.Router();
+
+// Imported model
+const { bookMovieModel } = require("./model");
+
 // get api to send last movie booking details
 movieRouter.get("/api/booking", async (req, res) => {
-    await connection;
     const lastRecord = await bookMovieModel.findOne().sort({ _id: -1 }).limit(1).select("-_id -__v");
 
     res.send(lastRecord ? lastRecord : { message: "no previous booking found" });
@@ -18,5 +17,5 @@ movieRouter.post("/api/booking", async (req, res) => {
     await bookMovieModel(req.body).save();
     res.send(req.body);
 });
-// Exported express router
+// Exported router
 module.exports = movieRouter;
