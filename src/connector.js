65f6111aabc67155a38acf41fcd6c9963db012bd
Ramesh_Chavan <ramesh7452@gmail.com>
2023-07-19 18:06:31 +0530

    self invoking function added

--- src/connector.js
+++ src/connector.js
@@ -5,20 +5,23 @@ const mongoose = require('mongoose');
 // Imported bookMovieSchema schema
 const { bookMovieSchema } = require('./schema');
 // To establish mongoose server connection
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => { console.log("connection established with mongodb server online"); })
-    .catch(err => {
-        console.log("error while connection", err)
-    });
-// Model created using book movie schema
+const conn = (async () => {
+    await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => { console.log("connection established with mongodb server online"); })
+        .catch(err => {
+            console.log("error while connection", err)
+        });
+    // Model created using book movie schema
 
-mongoose.connection.on('connected', () => {
-    console.log('Mongoose is connected.')
-});
+    // mongoose.connection.on('connected', () => {
+    //     console.log('Mongoose is connected.')
+    // });
 
-mongoose.connection.on('error', (err) => {
-    console.log(`Mongoose not connected: ${err}`)
-});
+    // mongoose.connection.on('error', (err) => {
+    //     console.log(`Mongoose not connected: ${err}`)
+    // });
+})();
+
 const collection_connection = mongoose.model('bookmovietickets', bookMovieSchema);
 // Exported model 
 exports.connection = collection_connection;
