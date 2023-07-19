e2740c3fc9c467ae0b53740402d118712d46c418
Ramesh_Chavan <ramesh7452@gmail.com>
2023-07-19 22:49:58 +0530

    model and connection added

--- src/api.js
+++ src/api.js
@@ -1,18 +1,21 @@
+const { connection } = require("./connector");
+
 // Imported express
 const express = require("express");
 // Imported connection model
-const { connection } = require("./connector");
+const { bookMovieModel } = require("./model");
 // Created express router
 const movieRouter = express.Router();
 // get api to send last movie booking details
 movieRouter.get("/api/booking", async (req, res) => {
-    const lastRecord = await connection.findOne().sort({ _id: -1 }).limit(1).select("-_id -__v");
-   
+    await connection;
+    const lastRecord = await bookMovieModel.findOne().sort({ _id: -1 }).limit(1).select("-_id -__v");
+
     res.send(lastRecord ? lastRecord : { message: "no previous booking found" });
 });
 // post api to save movie booking
 movieRouter.post("/api/booking", async (req, res) => {
-    await connection(req.body).save();
+    await bookMovieModel(req.body).save();
     res.send(req.body);
 });
 // Exported express router
